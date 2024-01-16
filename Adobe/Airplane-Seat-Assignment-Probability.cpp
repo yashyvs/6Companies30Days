397f590f273afe7374f6e7d426217743b3c2512e
Yash Vardhan Singh <yashvardhan.yvs21@gmail.com>
2024-01-16 19:36:28 +0530

    Create Airplane-Seat-Assignment-Probability.cpp

---
+++ Adobe/Airplane-Seat-Assignment-Probability.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        if(n == 1) return 1;
+        return (double)1/2;
+    }
+};
+
+
+/*
+* Ques Link : https://leetcode.com/problems/airplane-seat-assignment-probability/
+* Solution Link : https://leetcode.com/problems/airplane-seat-assignment-probability/submissions/1147864197/
+*/
