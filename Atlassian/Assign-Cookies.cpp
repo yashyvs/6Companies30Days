c5ad13c9ee925fee2b144331eb15b48a4e5f23cd
Yash Vardhan Singh <yashvardhan.yvs21@gmail.com>
2024-01-20 23:55:34 +0530

    Create Assign-Cookies.cpp

---
+++ Atlassian/Assign-Cookies.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int i = 0, j = 0;
+        int ans = 0;
+        while(i < g.size() && j < s.size()) {
+            if(g[i] <= s[j]) {
+                ans++;
+                i++;
+                j++;
+            }
+            else {
+                j++;
+            }
+        }
+        return ans;
+    }
+};
+
+
+/*
+* Ques Link : https://leetcode.com/problems/assign-cookies/
+* Solution Link : https://leetcode.com/problems/assign-cookies/submissions/1151861513/
+*/
